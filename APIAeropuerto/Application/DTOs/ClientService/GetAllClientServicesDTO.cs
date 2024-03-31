648f8b8ac43ff95e90c60fe3bd322bc2db5a7637
Eisler2712 <eislervalles@gmail.com>
2024-03-31 19:16:04 -0400

    "AddClientServices"

---
+++ APIAeropuerto/Application/DTOs/ClientService/GetAllClientServicesDTO.cs
@@ -0,0 +1,10 @@
+﻿using APIAeropuerto.Application.DTOs.Services;
+
+namespace APIAeropuerto.Application.DTOs.ClientService;
+
+public class ClientServicesDTO : ServiceDTO
+{
+    public Guid Id { get; set; }
+    public string Comments { get; set; }
+    public float Rating { get; set; }
+}
--- APIAeropuerto/Persistence/Mappers/ClientServicesMapper.cs
+++ APIAeropuerto/Persistence/Mappers/ClientServicesMapper.cs
@@ -1,4 +1,6 @@
-﻿using APIAeropuerto.Domain.Entities;
+﻿using APIAeropuerto.Application.DTOs.ClientService;
+using APIAeropuerto.Application.DTOs.Services;
+using APIAeropuerto.Domain.Entities;
 using APIAeropuerto.Persistence.Entities;
 using AutoMapper;
 
@@ -10,5 +12,13 @@ public class ClientServicesMapper : Profile
     {
         CreateMap<ClientServicesEntity, ClientServicesPersistence>();
         CreateMap<ClientServicesPersistence, ClientServicesEntity>();
+        CreateMap<ClientServicesPersistence, ClientServicesDTO>()
+            .ForMember(x => x.Type, y => y.MapFrom(z => z.Service.ServiceType.Type))
+            .ForMember(x => x.Code, y => y.MapFrom(z => z.Service.Code))
+            .ForMember(x => x.Id, y => y.MapFrom(z => z.IdService))
+            .ForMember(x => x.InstallationName, y => y.MapFrom(z => z.Service.Installation.Name))
+            .ForMember(x => x.IdInstallations, y => y.MapFrom(z => z.Service.Installation.Id))
+            .ForMember(x => x.Description, y => y.MapFrom(z => z.Service.Description))
+            .ForMember(x => x.Price, y => y.MapFrom(z => z.Service.Price));
     }
 }
